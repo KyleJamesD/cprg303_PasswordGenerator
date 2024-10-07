72637792b006ef1dac75d03cd84c526327d1536b
kyledyck@outlook.com <144828203+KyleJamesD@users.noreply.github.com>
2024-10-07 13:25:18 -0600

    Second commit input,check box, buttons mostly complete

--- src/Main.tsx
+++ src/Main.tsx
@@ -5,16 +5,85 @@ import {
     StyleSheet,
 } from "react-native";
 import InputBox from "./components/InputBox";
-
+import { useState } from "react";
+import CheckBox from "./components/FormCheckBox";
+import Btn from "./components/Btn";
+import Output from "./components/Output";
+import { btnType } from "./components/Btn";
 
 
 
 function Main() : React.JSX.Element {
+
+
+//***********************CheckBox Functions**************************** */
+const [checkboxVal1, setCheckboxVal1] = useState(false);
+function updateCheckBox1(status : boolean) {
+    console.log(`checked box1 status: ${status}`);
+    setCheckboxVal1(status);
+}
+
+const [checkboxVal2, setCheckboxVal2] = useState(false);
+function updateCheckBox2(status : boolean) {
+    console.log(`checked box2 status: ${status}`);
+    setCheckboxVal2(status);
+}
+
+const [checkboxVal3, setCheckboxVal3] = useState(false);
+function updateCheckBox3(status : boolean) {
+    console.log(`checked box3 status: ${status}`);
+    setCheckboxVal3(status);
+}
+
+const [checkboxVal4, setCheckboxVal4] = useState(false);
+function updateCheckBox4(status : boolean) {
+    console.log(`checked box4 status: ${status}`);
+    setCheckboxVal4(status);
+}
+//********************************************************************************* */
+
+//***************************InputBox********************************** */
+const [userInputVal, setUserInputVal] = useState('');
+//********************************************************************************* */
+
+//**************************Button functions ************************* */
+
+function ButtonPress1 () {
+    console.log("Generate Password pressed");
+}
+function ButtonPress2 () {
+    console.log("Reset Button  pressed");
+}
+
+//**************************Will we need this?************************* */
+const [outputState, updateOutputState] = useState({
+    inputVal : '',
+    checkedVal1 : '',
+});
+//********************************************************************************* */
+
+
+
     return (
+    <View>
         <View style={style.container}>
             <Text style={style.header}>Password Generator</Text>
             <InputBox></InputBox>
+            
         </View>
+        <View>
+            <CheckBox label="Upper Case Letter" checkedStatus={checkboxVal1} updateCheckedStatus={updateCheckBox1} />
+            <CheckBox label="Lower Case Letter" checkedStatus={checkboxVal2} updateCheckedStatus={updateCheckBox2} />
+            <CheckBox label="Special Character" checkedStatus={checkboxVal3} updateCheckedStatus={updateCheckBox3} />
+            <CheckBox label="Numbers" checkedStatus={checkboxVal4} updateCheckedStatus={updateCheckBox4} />
+        </View>
+
+
+        <View>
+            <Btn type={btnType.Primary} title="Generate Password" onPress={ButtonPress1}></Btn>
+            <Btn type={btnType.Danger} title="Reset" onPress={ButtonPress2}></Btn>
+        </View>
+    </View>
     );
 }
     
